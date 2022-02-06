40d5e9f6f6c92c6a79e90f1ef87ce83e0387eef2
Dylan <dburston0@gmail.com>
2022-02-06 13:37:53 -0500

    first commit pog collector

---
+++ main_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return render(request, 'home.html') 
+
+def index(request):
+    return render(request, 'index.html') 
